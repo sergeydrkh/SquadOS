94b2a6633ddf9a41646a7325db36225633ecfad8
s3r3zka <serezadorohin@gmail.com>
2020-06-24 18:28:58 +0300

    [-]

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,9 +1,12 @@
+import os.Main;
+
 public class Start {
     public static void main(String[] args) {
         new Start().run();
     }
 
     private void run() {
-
+        Thread mainThread = new Main();
+        mainThread.start();
     }
 }
---
+++ src/main/java/os/Main.java
@@ -0,0 +1,9 @@
+package os;
+
+public class Main extends Thread {
+
+    @Override
+    public void start() {
+        System.out.println("Started!");
+    }
+}
