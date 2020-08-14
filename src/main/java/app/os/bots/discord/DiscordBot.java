41bae907fb151050edcd5c724d42be90a421754c
Stepan Aleskeevich <serezadorohin@gmail.com>
2020-08-14 14:01:57 +0700

    v0.3.3-stable
    - removed `adm` prefix
    - added `!` prefix
    - other fixes

--- src/main/java/app/os/bots/discord/DiscordBot.java
+++ src/main/java/app/os/bots/discord/DiscordBot.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class DiscordBot {
     // discord bot info
     public static final String NAME = "SquadOS";
-    public static final String VERSION = "0.3.2_stable";
+    public static final String VERSION = "0.3.3_stable";
     public static final String OWNER_ID = "662324806187745290";
     private static final String TOKEN = "Njk5NTg4NzgzNDA1NzkzMzIz.XvN3vw.8qP5htUlzBVxo3QrCM9DAFRaLQM";
 
@@ -45,114 +45,111 @@ public class DiscordBot {
             // receive
             Message received = event.getMessage();
             String rawData = received.getContentRaw();
+            String[] args = rawData.split(" ");
 
-            if (rawData.matches("^(invite|инвайт|приглашение|inviteLink|ссылка)"))
+            if (!rawData.startsWith("!"))
+                return;
+
+            if (rawData.contains("link"))
                 received.getChannel().sendMessage(String.format("**Ссылка для приглашения:** %n%n%s%n%s%n%s", getInviteLink(), getInviteLink(), getInviteLink())).queue();
 
             // process data
-            if ((rawData.startsWith("adm") || rawData.startsWith("адм")) && rawData.split(" ").length >= 2) {
-                List<Role> adminRoles = received.getGuild().getRolesByName("admin", true);
-                if (!Objects.requireNonNull(received.getMember()).getRoles().containsAll(adminRoles) || !Objects.requireNonNull(event.getMember()).isOwner())
-                    return;
+            List<Role> adminRoles = received.getGuild().getRolesByName("admin", true);
+            if (!Objects.requireNonNull(received.getMember()).getRoles().containsAll(adminRoles))
+                return;
 
-                String command = rawData.split(" ")[1];
+            String command = args[0];
 
-                // other commands
-                if (command.matches("^(clear|очистить|очистить-чат)")) {
-                    clearMessages(received.getTextChannel());
-                    return;
-                }
+            // other commands
+            if (command.contains("clear")) {
+                AdminCommands.clearMessages(received.getTextChannel());
+                return;
+            }
 
-                // users management
-                List<Member> mentionedMembers = received.getMentionedMembers();
-                if (mentionedMembers.isEmpty()) {
-                    received.getChannel().sendMessage("Ошибка! Вы не указали **пользователей**!").queue();
-                    return;
-                }
 
-                if (command.matches("^(warn|пред|варн)")) {
+            // users management
+            List<Member> mentionedMembers = received.getMentionedMembers();
+            if (!mentionedMembers.isEmpty()) {
+                if (command.contains("warn")) {
                     for (Member memberToWarn : mentionedMembers) {
-                        warnUser(memberToWarn);
+                        AdminCommands.warnUser(memberToWarn);
                         received.getChannel().sendMessage(String.format("<@%s> получил **варн**!", memberToWarn.getId())).queue();
                     }
-                    return;
-                }
-
-                if (command.matches("^(tempban|временный-бан|снятие-ролей)")) {
+                } else if (command.contains("ban")) {
                     for (Member memberToTimeBan : mentionedMembers) {
-                        timeBan(memberToTimeBan);
+                        AdminCommands.timeBan(memberToTimeBan);
                         received.getChannel().sendMessage(String.format("<@%s> получил **временный** бан! (снятие ролей)", memberToTimeBan.getId())).queue();
                     }
-                    return;
-                }
-
-                if (command.matches("^(permban|перманент|пермач)")) {
+                } else if (command.contains("permban")) {
                     for (Member memberToPermBan : mentionedMembers) {
-                        permBan(memberToPermBan);
+                        AdminCommands.permBan(memberToPermBan);
                         received.getChannel().sendMessage(String.format("<@%s> получил **перманентный** бан!", memberToPermBan.getId())).queue();
                     }
-                    return;
-                }
-
-                if (command.matches("^(verify|верификация)")) {
+                } else if (command.contains("verify")) {
                     for (Member memberToVerify : mentionedMembers) {
-                        verify(memberToVerify);
+                        AdminCommands.verify(memberToVerify);
                         received.getChannel().sendMessage(String.format("<@%s> прошел **верефикацию**.", memberToVerify.getId())).queue();
                     }
-                    return;
-                }
-
-                if (command.matches("^(unwarn|снять-варны)")) {
+                } else if (command.contains("unwarn")) {
                     for (Member memberToUnWarn : mentionedMembers) {
-                        unWarn(memberToUnWarn);
-                        received.getChannel().sendMessage(String.format("С <@%s> сняты **все** варны.", memberToUnWarn.getId())).queue();
+                        AdminCommands.unWarn(memberToUnWarn);
+                        received.getChannel().sendMessage(String.format("У <@%s> сняты **все** варны.", memberToUnWarn.getId())).queue();
                     }
-                    return;
+                } else {
+                    received.getChannel().sendMessage("**Неизвестная** команда!").queue();
                 }
             }
         }
 
-        private void warnUser(Member member) {
-            List<Role> addWarns = member.getGuild().getRoles().stream().filter(role -> role.getName().toLowerCase().contains("warn") && !member.getRoles().contains(role)).collect(Collectors.toList());
+        private static class AdminCommands {
+            private static void warnUser(Member member) {
+                List<Role> addWarns = member.getGuild().getRoles().stream().filter(role -> role.getName().toLowerCase().contains("warn") && !member.getRoles().contains(role)).collect(Collectors.toList());
 
-            if (!addWarns.isEmpty())
-                member.getGuild().addRoleToMember(member.getUser().getId(), addWarns.get(addWarns.size() - 1)).queue();
-            else timeBan(member);
-        }
+                if (!addWarns.isEmpty())
+                    member.getGuild().addRoleToMember(member.getUser().getId(), addWarns.get(addWarns.size() - 1)).queue();
+                else timeBan(member);
+            }
 
-        private void timeBan(Member member) {
-            member.getRoles().stream().filter(role -> !role.getName().toLowerCase().contains("ban")).forEach(role -> member.getGuild().removeRoleFromMember(member.getUser().getId(), role).queue());
-        }
+            private static void timeBan(Member member) {
+                member.getRoles().stream().filter(role -> !role.getName().toLowerCase().contains("ban")).forEach(role -> member.getGuild().removeRoleFromMember(member.getUser().getId(), role).queue());
+            }
 
-        private void permBan(Member member) {
-            member.ban(1, "WARNS").queue();
-        }
+            private static void permBan(Member member) {
+                member.ban(1, "WARNS").queue();
+            }
 
-        private void verify(Member member) {
-            member.getGuild().getRoles().stream().filter(role -> role.getName().toLowerCase().contains("verified")).forEach(role -> member.getGuild().addRoleToMember(member.getUser().getId(), role).queue());
-        }
+            private static void verify(Member member) {
+                member.getGuild().getRoles().stream().filter(role -> role.getName().toLowerCase().contains("verified")).forEach(role -> member.getGuild().addRoleToMember(member.getUser().getId(), role).queue());
+            }
 
-        private void unWarn(Member member) {
-            member.getGuild().getRoles().stream().filter(role -> role.getName().toLowerCase().contains("warn")).forEach(role -> member.getGuild().removeRoleFromMember(member.getUser().getId(), role).queue());
-        }
+            private static void unWarn(Member member) {
+                member.getGuild().getRoles().stream().filter(role -> role.getName().toLowerCase().contains("warn")).forEach(role -> member.getGuild().removeRoleFromMember(member.getUser().getId(), role).queue());
+            }
 
-        private void clearMessages(TextChannel channel) {
-            AtomicBoolean isWorking = new AtomicBoolean(true);
-            OffsetDateTime twoWeeksAgo = OffsetDateTime.now().minus(2, ChronoUnit.WEEKS);
+            private static void clearMessages(TextChannel channel) {
+                AtomicBoolean isWorking = new AtomicBoolean(true);
+                OffsetDateTime twoWeeksAgo = OffsetDateTime.now().minus(2, ChronoUnit.WEEKS);
 
-            new Thread(() -> {
-                while (isWorking.get()) {
-                    List<Message> messages = channel.getHistory().retrievePast(50).complete();
-                    messages.removeIf(m -> m.getTimeCreated().isBefore(twoWeeksAgo));
+                new Thread(() -> {
+                    while (isWorking.get()) {
+                        List<Message> messages = channel.getHistory().retrievePast(50).complete();
+                        messages.removeIf(m -> m.getTimeCreated().isBefore(twoWeeksAgo));
 
-                    if (messages.isEmpty()) {
-                        isWorking.set(false);
-                        return;
+                        if (messages.isEmpty()) {
+                            isWorking.set(false);
+                            return;
+                        }
+
+                        try {
+                            channel.deleteMessages(messages).complete();
+                        } catch (java.lang.IllegalArgumentException e) {
+                            channel.sendMessage("Произошла **ошибка** при выполнении операции!\n\n" + "``" + e.getMessage() + "``").queue();
+                            return;
+                        }
+
                     }
-
-                    channel.deleteMessages(messages).complete();
-                }
-            }).start();
+                }).start();
+            }
         }
     }
 
