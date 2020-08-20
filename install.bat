0612da365c35b6661344a43dd5a5b0e278d93784
Stepan Aleskeevich <serezadorohin@gmail.com>
2020-08-21 01:20:15 +0700

    0.5 FINAL
    - added installer (install.bat)

---
+++ install.bat
@@ -0,0 +1,15 @@
+@echo off
+echo starting...
+cd C:\\
+
+echo creating squados folder...
+mkdir squados
+cd squados\\
+
+echo creating data folder...
+mkdir data
+
+echo Installed succesfully
+echo Launch SquadOS and fill ds.properties
+
+set /p press=Press enter to exit:
