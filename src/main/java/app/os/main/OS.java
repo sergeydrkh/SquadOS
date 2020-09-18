cd161e2afa64ea8db7819326e40f77f818c23f0f
Stepan Aleskeevich <serezadorohin@gmail.com>
2020-09-19 01:49:12 +0700

    0.6.5
    - localization (ru), now all SquadOS is russian!
    - fixed bugs
    - code cleanup
    - updated Info.java command
    - started to modern Command.java class
    - removed unused json-simple from pom.xml

--- src/main/java/app/os/main/OS.java
+++ src/main/java/app/os/main/OS.java
@@ -19,7 +19,7 @@ public class OS extends Thread {
 
     public static final Date DATE_LAUNCH = new Date();
     public static final String NAME = "SquadOS";
-    public static final String VERSION = "0.6.1";
+    public static final String VERSION = "0.6.5";
     public static final String DESCRIPTION = "SquadOS - a simple and easy to use multifunctional Discord bot. Music, moderation, fun - ez!";
 
     @Override
